51c1b4221b77fe80a4a0d1228d91be157fce5725
Conor Damery <mike@alonica.net>
2017-11-23 00:39:28 +0100

    bug fixes, added dragon model

--- src/main.cpp
+++ src/main.cpp
@@ -26,12 +26,12 @@
 #include "tinyfiledialogs.h"
 
 #define WIN_TITLE "Point Cloud Viewer"
-#define WIN_WIDTH 640
+#define WIN_WIDTH 1024
 #define WIN_HEIGHT 480
 #define GL_MAJOR 3
 #define GL_MINOR 3
 #define VSYNC 1 // Use if supported
-#define MSAA 4
+#define MSAA 2
 
 /////////////
 // Shaders //
@@ -56,7 +56,7 @@ static const char *shape_frag =
 
 // For point cloud meshes
 static const char *pointcloud_vert =
-    "#version 330 core\n\
+"#version 330 core\n\
     layout(location = 0) in vec3 POSITION;\n\
     layout(location = 1) in vec3 NORMAL;\n\
     out vec3 _Normal;\n\
@@ -67,15 +67,24 @@ static const char *pointcloud_vert =
     }\n";
 
 static const char *pointcloud_frag =
-    "#version 330 core\n\
+"#version 330 core\n\
     in vec3 _Normal;\n\
     out vec4 frag;\n\
+	uniform int DrawMode;\n\
+	uniform float LightIntensity;\n\
     uniform vec3 LightDir;\n\
-    uniform vec4 LightCol;\n\
-    uniform vec4 AmbientCol;\n\
+    uniform vec3 LightCol;\n\
+    uniform vec3 DiffuseCol;\n\
+    uniform vec3 AmbientCol;\n\
     void main() {\n\
-        float d = dot(_Normal, normalize(-LightDir));\n\
-        frag = AmbientCol + d * LightCol;\n\
+		if (DrawMode == 0) {\n\
+			frag = vec4(DiffuseCol, 1);\n\
+		} else if (DrawMode == 1) {\n\
+			frag = vec4(abs(normalize(_Normal)), 1);\n\
+		} else {\n\
+			float d = dot(_Normal, normalize(-LightDir));\n\
+			frag = vec4(AmbientCol + d * LightIntensity * LightCol * DiffuseCol, 1);\n\
+		}\n\
     }\n";
 
 ////////////////////////////
@@ -84,13 +93,13 @@ static const char *pointcloud_frag =
 
 static void error_callback(int error, const char* description)
 {
-    fprintf(stderr, "Error: %s\n", description);
+	fprintf(stderr, "Error: %s\n", description);
 }
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
 ///////////////////////
@@ -102,60 +111,60 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 */
 static bool createShader(GLuint &prog, const char *vertSrc, const char *fragSrc)
 {
-    GLint success = 0;
-    GLchar errBuff[1024] = { 0 };
+	GLint success = 0;
+	GLchar errBuff[1024] = { 0 };
 
 	// Create vertex shader
-    GLuint vert = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vert, 1, &vertSrc, NULL);
-    glCompileShader(vert);
+	GLuint vert = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vert, 1, &vertSrc, NULL);
+	glCompileShader(vert);
 
-    glGetShaderiv(vert, GL_COMPILE_STATUS, &success);
-    if (success == GL_FALSE) {
-        glGetShaderInfoLog(vert, sizeof(errBuff), NULL, errBuff);
-        std::cerr << errBuff << std::endl;
-        return false;
-    }
+	glGetShaderiv(vert, GL_COMPILE_STATUS, &success);
+	if (success == GL_FALSE) {
+		glGetShaderInfoLog(vert, sizeof(errBuff), NULL, errBuff);
+		std::cerr << errBuff << std::endl;
+		return false;
+	}
 
 	// Create fragment shader
-    GLuint frag = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(frag, 1, &fragSrc, NULL);
-    glCompileShader(frag);
+	GLuint frag = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(frag, 1, &fragSrc, NULL);
+	glCompileShader(frag);
 
-    glGetShaderiv(frag, GL_COMPILE_STATUS, &success);
-    if (success == GL_FALSE) {
-        glGetShaderInfoLog(frag, sizeof(errBuff), NULL, errBuff);
-        std::cerr << errBuff << std::endl;
-        return false;
-    }
+	glGetShaderiv(frag, GL_COMPILE_STATUS, &success);
+	if (success == GL_FALSE) {
+		glGetShaderInfoLog(frag, sizeof(errBuff), NULL, errBuff);
+		std::cerr << errBuff << std::endl;
+		return false;
+	}
 
 	// Create shader program
-    prog = glCreateProgram();
-    glAttachShader(prog, vert);
-    glAttachShader(prog, frag);
+	prog = glCreateProgram();
+	glAttachShader(prog, vert);
+	glAttachShader(prog, frag);
 
-    glLinkProgram(prog);
+	glLinkProgram(prog);
 
-    glGetProgramiv(prog, GL_LINK_STATUS, &success);
-    if (success == GL_FALSE) {
-        glGetShaderInfoLog(frag, sizeof(errBuff), NULL, errBuff);
-        std::cerr << errBuff << std::endl;
-        return false;
-    }
+	glGetProgramiv(prog, GL_LINK_STATUS, &success);
+	if (success == GL_FALSE) {
+		glGetShaderInfoLog(frag, sizeof(errBuff), NULL, errBuff);
+		std::cerr << errBuff << std::endl;
+		return false;
+	}
 
-    glValidateProgram(prog);
+	glValidateProgram(prog);
 
-    glGetProgramiv(prog, GL_LINK_STATUS, &success);
-    if (success == GL_FALSE) {
-        glGetShaderInfoLog(frag, sizeof(errBuff), NULL, errBuff);
-        std::cerr << errBuff << std::endl;
-        return false;
-    }
+	glGetProgramiv(prog, GL_LINK_STATUS, &success);
+	if (success == GL_FALSE) {
+		glGetShaderInfoLog(frag, sizeof(errBuff), NULL, errBuff);
+		std::cerr << errBuff << std::endl;
+		return false;
+	}
 
 	// No need for these so we can delete them
-    glDeleteShader(vert);
-    glDeleteShader(frag);
-    return true;
+	glDeleteShader(vert);
+	glDeleteShader(frag);
+	return true;
 }
 
 /**
@@ -268,7 +277,7 @@ void loadScene(const std::string filename, GLuint &bounds, std::vector<std::pair
 * Handles the "load scene" event.
 */
 void loadSceneFile(GLuint &bounds, std::vector<std::pair<GLuint, size_t>> &meshes) {
-    const char *filename = tinyfd_openFileDialog("Open", "", 0, NULL, "scene files", 0);
+	const char *filename = tinyfd_openFileDialog("Open", "", 0, NULL, "scene files", 0);
 	if (filename != NULL) {
 		// Deletes buffers if any was created
 		if (bounds != 0) {
@@ -279,7 +288,7 @@ void loadSceneFile(GLuint &bounds, std::vector<std::pair<GLuint, size_t>> &meshe
 		}
 		bounds = 0;
 		meshes.clear();
-		
+
 		// Loads the scene meshes
 		loadScene(filename, bounds, meshes);
 	}
@@ -292,102 +301,115 @@ void loadSceneFile(GLuint &bounds, std::vector<std::pair<GLuint, size_t>> &meshe
 int main(int argc, char ** args)
 {
 	// Create window
-    GLFWwindow* window;
-    glfwSetErrorCallback(error_callback);
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_MAJOR);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GL_MINOR);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We are using the core profile for GLSL 330
+	GLFWwindow* window;
+	glfwSetErrorCallback(error_callback);
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_MAJOR);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GL_MINOR);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We are using the core profile for GLSL 330
 	glfwWindowHint(GLFW_SAMPLES, MSAA);
-    window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE, NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
-    glfwSetKeyCallback(window, key_callback);
-    glfwMakeContextCurrent(window);
-    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
-    glfwSwapInterval(VSYNC);
+	window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE, NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	glfwSetKeyCallback(window, key_callback);
+	glfwMakeContextCurrent(window);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	glfwSwapInterval(VSYNC);
 
-    // Setup ImGui binding
-    ImGui_ImplGlfwGL3_Init(window, true);
+	// Setup ImGui binding
+	ImGui_ImplGlfwGL3_Init(window, true);
 
 	// OpenGL config
-    printf("%s\n", glGetString(GL_VERSION));
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_DEPTH_CLAMP);
-    glEnable(GL_MULTISAMPLE);
-    glDisable(GL_CULL_FACE);
+	printf("%s\n", glGetString(GL_VERSION));
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_CLAMP);
+	glEnable(GL_MULTISAMPLE);
+	glDisable(GL_CULL_FACE);
 
 	// Rendering vars
 	GLuint bounds = 0;
 	std::vector<std::pair<GLuint, size_t>> meshes;
 
-    GLuint pointcloundShader;
-    createShader(pointcloundShader, pointcloud_vert, pointcloud_frag);
+	GLuint pointcloundShader;
+	createShader(pointcloundShader, pointcloud_vert, pointcloud_frag);
 
-    GLuint shapeShader;
-    createShader(shapeShader, shape_vert, shape_frag);
+	GLuint shapeShader;
+	createShader(shapeShader, shape_vert, shape_frag);
 
 	// Window vars
-    float ratio;
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
-    ratio = width / (float) height;
+	float ratio;
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	ratio = width / (float)height;
 
 	// Defines our global axis
-    const glm::vec3 right(1, 0, 0);
-    const glm::vec3 up(0, 1, 0);
-    const glm::vec3 forward(0, 0, 1);
+	const glm::vec3 right(1, 0, 0);
+	const glm::vec3 up(0, 1, 0);
+	const glm::vec3 forward(0, 0, 1);
+
+	const float half_pi = 3.1415 * 0.5f;
 
 	// Shader vars
-    glm::vec3 lightDir(0, -1, 0);
-    glm::vec4 lightCol(1, 1, 1, 1);
-    glm::vec4 ambientCol(0.01, 0.01, 0.01, 1);
+	int drawMode = 3;
+	float lightIntensity = 1.0f;
+	glm::vec3 lightDir(0, -1, 0);
+	glm::vec3 lightCol(1, 1, 1);
+	glm::vec3 diffuseCol(1, 1, 1);
+	glm::vec3 ambientCol(0.01, 0.01, 0.01);
 
-    glm::vec4 boundsColor(0, 1, 0, 0.5f);
+	glm::vec4 boundsColor(0, 1, 0, 0.5f);
 
 	// Camera control vars
-    glm::vec3 camPos(0, 0, -1);
-    glm::quat camRot; // Used to transform forward dir
-    glm::vec3 move(0);
-    glm::vec3 angles(0); // Euler angles
+	glm::vec3 camPos(-12.5, 7.0f, -10.0f);
+	glm::quat camRot(-0.83, 0.14, 0.53, -0.09); // Used to transform forward dir
+	glm::vec3 move(0);
+	glm::vec3 angles = glm::eulerAngles(camRot);
 
-    glm::mat4 projT = glm::perspective(70.f, ratio, 0.1f, 1000.f);
-    glm::mat4 viewT = glm::lookAt(camPos, camPos + forward * camRot, up); // Looks at the forward direction of rotated camera
-    glm::mat4 modelT = glm::scale(glm::vec3(2));
-    glm::mat4 mvpT;
+	glm::mat4 projT = glm::perspective(70.f, ratio, 0.1f, 1000.f);
+	glm::mat4 viewT = glm::lookAt(camPos, camPos + forward * camRot, up); // Looks at the forward direction of rotated camera
+	glm::mat4 modelT = glm::scale(glm::vec3(2));
+	glm::mat4 mvpT;
 
-    glm::dvec2 mousePos;
-    glm::dvec2 mouseDelta;
-    glfwGetCursorPos(window, &mousePos.x, &mousePos.y);
+	glm::dvec2 mousePos;
+	glm::dvec2 mouseDelta;
+	glfwGetCursorPos(window, &mousePos.x, &mousePos.y);
 
 	// Game loop vars
-    auto start = std::chrono::high_resolution_clock::now();
-    auto end = start;
+	auto start = std::chrono::high_resolution_clock::now();
+	auto end = start;
 
 	std::chrono::duration<double, std::nano> frameTime(1000000000 / 60.0);
 	std::chrono::duration<double, std::nano> elapsed;
-    float delta = 0;
+	std::chrono::duration<double, std::nano> carry;
+	float delta = 0;
 
 	// Config vars
+	float mouseSensitivity = 0.7f;
+	float moveSensitivity = 2.0f;
 	float scaleExp = 0.9f;
 	bool scalePoints = true;
 	bool drawBounds = true;
+	bool vsync = VSYNC;
 
-    while (!glfwWindowShouldClose(window))
-    {
+	while (!glfwWindowShouldClose(window))
+	{
 		// Calculate delta frame time
-        end = std::chrono::high_resolution_clock::now();
-        elapsed = end - start;
-        delta = (end - start).count() / 1000000000.f;
-        start = end;
+		end = std::chrono::high_resolution_clock::now();
+		elapsed = (end - start) + carry;
+		delta = (end - start).count() / 1000000000.f;
+		start = end;
 
-        if (elapsed < frameTime)
-            std::this_thread::sleep_for(frameTime - elapsed);
+		if (elapsed < frameTime) {
+			auto startSleep = std::chrono::high_resolution_clock::now();
+			auto sleepTime = frameTime - elapsed;
+			std::this_thread::sleep_for(sleepTime);
+			carry = sleepTime - (std::chrono::high_resolution_clock::now() - startSleep);
+		}
 
-        // GUI input
+		// GUI input
 		ImGui_ImplGlfwGL3_NewFrame();
 
 		ImGui::BeginMainMenuBar();
@@ -395,17 +417,33 @@ int main(int argc, char ** args)
 			if (ImGui::MenuItem("Load Scene", "", false, true))
 				loadSceneFile(bounds, meshes);
 			ImGui::EndMenu();
-        }
+		}
+		if (ImGui::BeginMenu("Settings")) {
+			if (ImGui::Checkbox("VSync", &vsync))
+				glfwSwapInterval(vsync);
+			if (ImGui::InputFloat("Mouse Sensitivity", &mouseSensitivity, 0.01f, 0.1f, 2))
+				mouseSensitivity = glm::clamp(mouseSensitivity, 0.1f, 1.0f);
+			if (ImGui::InputFloat("Move Sensitivity", &moveSensitivity, 0.05f, 0.2f, 2))
+				moveSensitivity = glm::clamp(moveSensitivity, 0.1f, 10.0f);
+			ImGui::EndMenu();
+		}
 		ImGui::EndMainMenuBar();
 
 		ImGui::Begin("- Rendering -");
-		ImGui::InputFloat4("Ambient Col", const_cast<float *>(glm::value_ptr(ambientCol)), 2);
-		ImGui::InputFloat4("Light Col", const_cast<float *>(glm::value_ptr(lightCol)), 2);
+		ImGui::InputFloat3("Ambient Col", const_cast<float *>(glm::value_ptr(ambientCol)), 2);
+		ImGui::InputFloat3("Diffuse Col", const_cast<float *>(glm::value_ptr(diffuseCol)), 2);
+		ImGui::InputFloat3("Light Col", const_cast<float *>(glm::value_ptr(lightCol)), 2);
 		ImGui::InputFloat3("Light Dir", const_cast<float *>(glm::value_ptr(lightDir)), 2);
+		ImGui::InputFloat("Light Intensity", &lightIntensity, 0.01f, 0.1f, 2);
 		if (ImGui::Button("Normalize")) {
 			if (glm::dot(lightDir, lightDir) > 1)
 				lightDir = glm::normalize(lightDir);
 		}
+
+		ImGui::RadioButton("Unlit", &drawMode, 0);
+		ImGui::RadioButton("Normals", &drawMode, 1);
+		ImGui::RadioButton("Lit", &drawMode, 3);
+
 		ImGui::Checkbox("Bounds", &drawBounds);
 		ImGui::Checkbox("Scaled", &scalePoints);
 		if (scalePoints) {
@@ -414,88 +452,94 @@ int main(int argc, char ** args)
 		ImGui::End();
 
 		// Camera input
-        mouseDelta = mousePos;
-        glfwGetCursorPos(window, &mousePos.x, &mousePos.y);
-        mouseDelta = mousePos - mouseDelta;
+		mouseDelta = mousePos;
+		glfwGetCursorPos(window, &mousePos.x, &mousePos.y);
+		mouseDelta = mousePos - mouseDelta;
 
-        move.x = 0;
-        move.z = 0;
-        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-            move.z = 1;
-        else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-            move.z = -1;
-        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-            move.x = 1;
-        else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-            move.x = -1;
+		move.x = 0;
+		move.z = 0;
+		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+			move.z = 1;
+		else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+			move.z = -1;
+		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+			move.x = 1;
+		else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+			move.x = -1;
 
-        // Update
-        if (glfwGetMouseButton(window, 1)) {
-            angles.x -= mouseDelta.y * delta;
-            angles.y += mouseDelta.x * delta;
-            camRot = glm::angleAxis(angles.x, right) * glm::angleAxis(angles.y, up);
-        }
+		// Update
+		if (glfwGetMouseButton(window, 1)) {
+			angles.y += glm::radians(mouseDelta.x) * mouseSensitivity;
+			angles.x -= glm::radians(mouseDelta.y) * mouseSensitivity;
+			angles.x = glm::clamp(angles.x, -half_pi, half_pi);
+			camRot = glm::angleAxis(angles.x, right) * glm::angleAxis(angles.y, up);
+		}
 
 		if (glm::dot(move, move) > 1)
 			move = glm::normalize(move);
-        camPos += 2.f * move * camRot * delta;
+		camPos += moveSensitivity * move * camRot * delta;
 
 		// Update MVP matrices
-        projT = glm::perspective(70.f, ratio, 0.1f, 1000.f);
-        viewT = glm::lookAt(camPos, camPos + forward * camRot, up);
-        mvpT = projT * viewT * modelT;
+		projT = glm::perspective(70.f, ratio, 0.1f, 1000.f);
+		viewT = glm::lookAt(camPos, camPos + forward * camRot, up);
+		mvpT = projT * viewT * modelT;
 
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
+		glfwGetFramebufferSize(window, &width, &height);
+		ratio = width / (float)height;
 
-        // Draw
-        glViewport(0, 0, width, height);
+		// Draw
+		glViewport(0, 0, width, height);
 
-        glClearColor(0.1f, 0.1f, 0.1f, 1);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClearColor(0.1f, 0.1f, 0.1f, 1);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Update point cloud shader
-        glUseProgram(pointcloundShader);
-        glUniformMatrix4fv(glGetUniformLocation(pointcloundShader, "MVP"), 1, GL_TRUE, glm::value_ptr(mvpT));
-        glUniform3fv(glGetUniformLocation(pointcloundShader, "LightDir"), 1, glm::value_ptr(lightDir));
-        glUniform4fv(glGetUniformLocation(pointcloundShader, "LightCol"), 1, glm::value_ptr(lightCol));
-        glUniform4fv(glGetUniformLocation(pointcloundShader, "AmbientCol"), 1, glm::value_ptr(ambientCol));
+		glUseProgram(pointcloundShader);
+		glUniformMatrix4fv(glGetUniformLocation(pointcloundShader, "MVP"), 1, GL_TRUE, glm::value_ptr(mvpT));
+		glUniform1f(glGetUniformLocation(pointcloundShader, "LightIntensity"), lightIntensity);
+		glUniform1i(glGetUniformLocation(pointcloundShader, "DrawMode"), drawMode);
+		glUniform3fv(glGetUniformLocation(pointcloundShader, "LightDir"), 1, glm::value_ptr(lightDir));
+		glUniform3fv(glGetUniformLocation(pointcloundShader, "LightCol"), 1, glm::value_ptr(lightCol));
+		glUniform3fv(glGetUniformLocation(pointcloundShader, "DiffuseCol"), 1, glm::value_ptr(diffuseCol));
+		glUniform3fv(glGetUniformLocation(pointcloundShader, "AmbientCol"), 1, glm::value_ptr(ambientCol));
+
 
 		if (scalePoints) {
 			const float size = (1.f / pow(glm::length(camPos), scaleExp)) * 20;
 			glPointSize(size);
-		} else {
+		}
+		else {
 			glPointSize(1.f);
 		}
 
-        for (auto mesh : meshes) {
-            glBindVertexArray(mesh.first);
-            glDrawArrays(GL_POINTS, 0, mesh.second);
-        }
+		for (auto mesh : meshes) {
+			glBindVertexArray(mesh.first);
+			glDrawArrays(GL_POINTS, 0, mesh.second);
+		}
 
 		// Update shape shader
-        glUseProgram(shapeShader);
-        glUniformMatrix4fv(glGetUniformLocation(shapeShader, "MVP"), 1, GL_TRUE, glm::value_ptr(mvpT));
-        glUniform4fv(glGetUniformLocation(shapeShader, "Color"), 1, glm::value_ptr(boundsColor));
+		glUseProgram(shapeShader);
+		glUniformMatrix4fv(glGetUniformLocation(shapeShader, "MVP"), 1, GL_TRUE, glm::value_ptr(mvpT));
+		glUniform4fv(glGetUniformLocation(shapeShader, "Color"), 1, glm::value_ptr(boundsColor));
 
 		if (bounds && drawBounds) {
 			glBindVertexArray(bounds);
 			glDrawElements(GL_LINES, 24, GL_UNSIGNED_INT, 0);
 		}
 
-        ImGui::Render();
+		ImGui::Render();
 
 		// Display
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
 	// Clean resources
-    for (auto mesh : meshes)
-        glDeleteVertexArrays(1, &mesh.first);
-    glDeleteVertexArrays(1, &bounds);
+	for (auto mesh : meshes)
+		glDeleteVertexArrays(1, &mesh.first);
+	glDeleteVertexArrays(1, &bounds);
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    exit(EXIT_SUCCESS);
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
 }
